be032d6bf500a26d33b2736a805549513cdd73a3
Estefania Garza <lorenagh910@hotmail.com>
2026-04-27 22:41:16 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const card = document.querySelector(".card");
+  const number = document.querySelector("#number");
+  const topSuit = document.querySelector(".top-suit");
+  const bottomSuit = document.querySelector(".bottom-suit");
+
+  const listSuits = [
+    {symbol: "♦", class: "diamond" }, 
+    {symbol: "♥", class: "heart" }, 
+    {symbol: "♠", class: "spade" }, 
+    {symbol: "♣", class: "club" }
+  ];
+  const randomSuit = listSuits[Math.floor(Math.random() * listSuits.length)];
+
+  const listNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const randomNumber = listNumber[Math.floor(Math.random() * listNumber.length)];
+
+  topSuit.innerText = randomSuit.symbol;
+  bottomSuit.innerText = randomSuit.symbol;
+  number.innerText = randomNumber;
+
+  card.className = "card";
+  card.classList.add(randomSuit.class);
+
+  console.log(randomSuit);
+
+
 };
